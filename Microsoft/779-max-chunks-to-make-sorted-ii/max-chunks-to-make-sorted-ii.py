770b8182a751f0161d56e04a355573f3d376e8f2
Atul Ahire <atulsureshahire@gmail.com>
2026-08-10 12:55:52 -0700

    Time: 5 ms (39.31%) | Memory: 19.3 MB (72.64%) - LeetSync

--- Microsoft/779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.py
+++ Microsoft/779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.py
@@ -3,15 +3,32 @@ class Solution:
         # naive solution
         # max(a[0..i]) <= min(a[i+1:])
 
-        n = len(arr)
-        chunk = 0
-        
-        for i in range(n):
-            left_max = max(arr[:i+1])
-            right_min = min(arr[i+1:]) if i + 1 < n else float('inf')
+        # n = len(arr)
+        # chunk = 0
 
-            if left_max <= right_min:
-                chunk += 1
+        # for i in range(n):
+        #     left_max = max(arr[:i+1])
+        #     right_min = min(arr[i+1:]) if i + 1 < n else float('inf')
+
+        #     if left_max <= right_min:
+        #         chunk += 1
         
-        return chunk
+        # return chunk
         
+        n = len(arr)
+        suffix_min = [0] * n
+        suffix_min[-1] = arr[-1]
+  
+        for i in range(n-2, -1, -1):
+            suffix_min[i] = min(arr[i], suffix_min[i+1])
+
+        chunks=0
+        prefix_max = float('-inf')
+
+        for i in range(n):
+            prefix_max = max(arr[i], prefix_max)
+
+            if i == n-1 or prefix_max <= suffix_min[i+1]:
+                chunks += 1
+        
+        return chunks

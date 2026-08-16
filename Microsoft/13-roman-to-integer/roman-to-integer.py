626391d4f8dfd1a4170861a536bbc3119e6942c3
Atul Ahire <atulsureshahire@gmail.com>
2026-08-15 17:05:36 -0700

    Time: 1 ms (87.39%) | Memory: 19.3 MB (57.67%) - LeetSync

--- Microsoft/13-roman-to-integer/roman-to-integer.py
+++ Microsoft/13-roman-to-integer/roman-to-integer.py
@@ -1,18 +1,33 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_hashmap = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'IV': 4, 'IX':9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+        roman_hashmap = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-        val = 0 
-        i = 0
+        # val = 0 
+        # i = 0
+        # n = len(s)
+
+        # while i < n:
+        #     if s[i] in ['I', 'X', 'C'] and i + 1 < n and s[i] + s[i+1] in roman_hashmap:
+        #         val += roman_hashmap[s[i] + s[i+1]]
+        #         i+= 1
+        #     else:
+        #         val += roman_hashmap[s[i]]
+
+        #     i+= 1
+
+        # return val
+
+        # pattern here is if s[i] < s[i+1], then value -= val[s[i]] else value += val[s[i]]
+
+
         n = len(s)
+        val = 0 
 
-        while i < n:
-            if s[i] in ['I', 'X', 'C'] and i + 1 < n and s[i] + s[i+1] in roman_hashmap:
-                val += roman_hashmap[s[i] + s[i+1]]
-                i+= 1
+        for i in range(n-1):
+            if roman_hashmap[s[i]] < roman_hashmap[s[i+1]]:
+                val -= roman_hashmap[s[i]]
             else:
                 val += roman_hashmap[s[i]]
 
-            i+= 1
-
-        return val      
+        
+        return val + roman_hashmap[s[-1]]

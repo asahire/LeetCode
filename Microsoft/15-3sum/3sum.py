1cc405552e43a06ff213aa675a34cc523723eb4a
Atul Ahire <atulsureshahire@gmail.com>
2026-08-16 22:04:56 -0700

    Time: 691 ms (27.06%) | Memory: 22.2 MB (53.90%) - LeetSync

--- Microsoft/15-3sum/3sum.py
+++ Microsoft/15-3sum/3sum.py
@@ -1,37 +1,44 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        # n = len(nums)
+        # res = set()
+
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         for k in range(j+1, n):
+        #             if nums[i] + nums[j] + nums[k] == 0:
+        #                 res.add(tuple(sorted((nums[i], nums[j], nums[k]))))
+        
+        # return [list(t) for t in res]
+        
         nums.sort()
         n = len(nums)
-        result = []
+        res = []
+       
         for i in range(n):
-            if nums[i] > 0:
-                break
-            elif i > 0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
             
-            left = i+1
-            right = n-1
-                
-            while left < right:
-                add = nums[i] + nums[left] + nums[right]
+            left, right = i+1, n-1
 
-                if add ==0:
-                    result.append([nums[i], nums[left], nums[right]])
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    res.append([nums[i], nums[left], nums[right]])
+                  
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
+
                     left += 1
                     right -= 1
 
-                    while left < right and nums[left] == nums[left-1]:
-                        left += 1
-                    
-                    while left < right and nums[right] == nums[right+1]:
-                        right -=1
-                elif add < 0:
 
-                    left += 1
-                else:
-                    right -=1
-                    
-        return result
-
-
-        
+        return res               

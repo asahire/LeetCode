3991e84eb8eb68e2749b87e742b0ba7eabb3c068
Atul Ahire <atulsureshahire@gmail.com>
2026-09-05 05:45:22 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (78.76%) - LeetSync

--- Microsoft/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ Microsoft/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,13 +1,36 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         pad = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
- 
-        combos = [""]
-
-        for d in digits:
-            combos = [prefix + ch for prefix in combos for ch in pad[d]]
         
-        return combos
+        # naive solution  O(n * 4^n), O(n)
+        # combos = [""]
+
+        # for d in digits:
+        #     combos = [prefix + ch for prefix in combos for ch in pad[d]]
+        
+        # return combos
+
+        res, path = [], []
+        def backtracking(i: int):
+            # base case
+            if i == len(digits):
+                res.append("".join(path))
+                return
+            
+            for ch in pad[digits[i]]:
+                path.append(ch)
+                backtracking(i+1)
+                path.pop()
+        
+        backtracking(0)
+
+        return res
+
+
+
+
+        
+        
 
 
         

91fa40edc38afcba344924415214784e912f6aff
Atul Ahire <atulsureshahire@gmail.com>
2026-09-07 12:39:17 -0700

    Time: 2 ms (38.82%) | Memory: 20.4 MB (47.14%) - LeetSync

--- Microsoft/25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ Microsoft/25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -5,24 +5,44 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        dummy = ListNode(0, head)
-        group_prev = dummy
+        curr, count = head, 0
 
-        while True:
-            kth = group_prev
-            for _ in range(k):
-                kth = kth.next
-                if not kth:
-                    return dummy.next
+        while curr and count < k:
+            curr = curr.next
+            count +=1
+        # base condition, not proportional to k, retain the order
+        if count < k:
+            return head
 
-            group_next = kth.next
+        prev = self.reverseKGroup(curr, k)
 
-            prev, curr = group_next, group_prev.next
-            while curr is not group_next:
-                curr.next, prev, curr = prev, curr, curr.next
+        
+        while count:
+            next = head.next
+            head.next = prev
+            prev = head
+            head = next
+            count -= 1
+        
+        return prev
+        # dummy = ListNode(0, head)
+        # group_prev = dummy
+
+        # while True:
+        #     kth = group_prev
+        #     for _ in range(k):
+        #         kth = kth.next
+        #         if not kth:
+        #             return dummy.next
+
+        #     group_next = kth.next
+
+            # prev, curr = group_next, group_prev.next
+            # while curr is not group_next:
+            #     curr.next, prev, curr = prev, curr, curr.next
             
 
-            tail = group_prev.next
-            group_prev.next = kth
-            group_prev = tail
+            # tail = group_prev.next
+            # group_prev.next = kth
+            # group_prev = tail
         

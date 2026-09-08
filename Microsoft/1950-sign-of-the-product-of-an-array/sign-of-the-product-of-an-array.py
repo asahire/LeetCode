ddbf22ba2d4a178474d14dc1bba76a1a048a9663
Atul Ahire <atulsureshahire@gmail.com>
2026-09-07 20:09:34 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (74.68%) - LeetSync

---
+++ Microsoft/1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        neg = 0
+
+        for n in nums:
+            if n==0:
+                return 0
+            
+            if n < 0:
+                neg += 1
+        
+        return -1 if neg % 2 else 1
+        

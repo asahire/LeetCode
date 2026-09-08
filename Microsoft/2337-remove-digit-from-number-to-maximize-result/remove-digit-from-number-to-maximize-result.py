a5cd6f90ba4c7f7e3d6f83dc8cae7511d3c91891
Atul Ahire <atulsureshahire@gmail.com>
2026-09-07 21:20:32 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (17.36%) - LeetSync

--- Microsoft/2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.py
+++ Microsoft/2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.py
@@ -1,13 +1,13 @@
 class Solution:
     def removeDigit(self, number: str, digit: str) -> str:
-        passed = 0
+        last = -1
 
         for i, ch in enumerate(number):
             if ch == digit:
-                passed = i
+                last = i
                 if i+1 < len(number) and number[i+1] > number[i]:
                     return number[:i] + number[i+1:]
                 
 
-        return number[:passed] + number[passed+1:]    
+        return number[:last] + number[last+1:]    
         

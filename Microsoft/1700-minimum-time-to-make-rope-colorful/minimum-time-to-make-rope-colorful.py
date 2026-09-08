8f7c6e1c96a2f523a9e151f034eb409e66351a6e
Atul Ahire <atulsureshahire@gmail.com>
2026-09-07 23:59:59 -0700

    Time: 63 ms (80.97%) | Memory: 29.1 MB (66.13%) - LeetSync

--- Microsoft/1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
+++ Microsoft/1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
@@ -3,10 +3,14 @@ class Solution:
 
         total = 0
         n = len(colors)
+        max_prev = neededTime[0]
         for i in range(1, n):
             if colors[i] == colors[i-1]:
-                total += min (neededTime[i], neededTime[i-1])
-                neededTime[i] = max(neededTime[i], neededTime[i-1])
+                total += min (max_prev, neededTime[i])
+                max_prev = max(max_prev, neededTime[i])
+            else:
+                max_prev = neededTime[i]
+
         return total
 
         

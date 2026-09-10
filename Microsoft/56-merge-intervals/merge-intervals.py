871b8aae48c915e35e59be23d97ba94699885728
Atul Ahire <atulsureshahire@gmail.com>
2026-09-10 04:06:16 -0700

    Time: 7 ms (73.02%) | Memory: 23 MB (42.27%) - LeetSync

---
+++ Microsoft/56-merge-intervals/merge-intervals.py
@@ -0,0 +1,15 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+
+        merged = [intervals[0]]
+
+        for start, end in intervals[1:]:
+            last = merged[-1]
+
+            if start <= last[1]:
+                last[1] = max(end, last[1])
+            else:
+                merged.append([start, end])
+        
+        return merged

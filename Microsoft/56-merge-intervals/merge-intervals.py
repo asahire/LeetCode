986a7a09c38c19883ea74a179edb24351207e1c8
Atul Ahire <atulsureshahire@gmail.com>
2026-09-10 04:07:45 -0700

    Time: 6 ms (77.86%) | Memory: 23.3 MB (7.73%) - LeetSync

--- Microsoft/56-merge-intervals/merge-intervals.py
+++ Microsoft/56-merge-intervals/merge-intervals.py
@@ -1,6 +1,6 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key= lambda iv: iv[0])
+        intervals.sort()
 
         merged = [intervals[0]]
 

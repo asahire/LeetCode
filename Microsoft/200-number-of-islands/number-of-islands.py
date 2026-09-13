0d2f442251e67b1b707d84b0f7947bccd6fe8f2a
Atul Ahire <atulsureshahire@gmail.com>
2026-09-12 20:05:30 -0700

    Time: 244 ms (55.16%) | Memory: 21.5 MB (72.99%) - LeetSync

--- Microsoft/200-number-of-islands/number-of-islands.py
+++ Microsoft/200-number-of-islands/number-of-islands.py
@@ -28,36 +28,50 @@ class Solution:
         #                         grid[nr][nc] = "0"
         
         # return num_of_islands
-
-        visited = [[False] * cols for _ in range(rows)]
-        num_of_islands= 0
-
-        def dfs(r, c):
-            if r < 0 or r >= rows or c < 0 or c >= cols:
-                return
-            if grid[r][c] == '0' or visited[r][c]:
-                return
-
-            visited[r][c] = True
-
-            dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r, c+1)
-            dfs(r, c-1)
-
-        for m in range(rows):
-            for n in range(cols):
-                if grid[m][n] == '1' and not visited[m][n]:
-                    num_of_islands += 1
-                    dfs(m,n)
         
-        return num_of_islands
+        # DFS - 1
+        # visited = [[False] * cols for _ in range(rows)]
+        # num_of_islands= 0
 
+        # def dfs(r, c):
+        #     if r < 0 or r >= rows or c < 0 or c >= cols:
+        #         return
+        #     if grid[r][c] == '0' or visited[r][c]:
+        #         return
 
+        #     visited[r][c] = True
 
+        #     dfs(r+1, c)
+        #     dfs(r-1, c)
+        #     dfs(r, c+1)
+        #     dfs(r, c-1)
 
+        # for m in range(rows):
+        #     for n in range(cols):
+        #         if grid[m][n] == '1' and not visited[m][n]:
+        #             num_of_islands += 1
+        #             dfs(m,n)
+        
+        # return num_of_islands
 
+        # DFS -2 
+        m, n = len(grid), len(grid[0])
+        count = 0
+        
+        def sink(r, c):
+            if r < 0 or r >= m or c < 0 or c >= n or grid[r][c] != '1':
+                return
+            grid[r][c] = '0'                      # mark visited by turning land into water
+            sink(r + 1, c)
+            sink(r - 1, c)
+            sink(r, c + 1)
+            sink(r, c - 1)
 
-
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == '1':
+                    count += 1
+                    sink(r, c)
+        return count
 
         

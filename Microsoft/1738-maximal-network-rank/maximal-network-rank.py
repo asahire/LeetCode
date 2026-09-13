363542adf3c37398665a0cc51bb0984ec0b440cd
Atul Ahire <atulsureshahire@gmail.com>
2026-09-13 00:15:13 -0700

    Time: 29 ms (50.98%) | Memory: 21 MB (15.12%) - LeetSync

--- Microsoft/1738-maximal-network-rank/maximal-network-rank.py
+++ Microsoft/1738-maximal-network-rank/maximal-network-rank.py
@@ -1,15 +1,30 @@
 class Solution:
     def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
         # naive
+        # best = 0
+        # for u in range(n):
+        #     for v in range(u+1, n):
+        #         rank = 0
+        #         for a, b in roads:
+        #             if a == u or a == v or b == u or b == v:
+        #                 rank += 1
+                
+        #         best = max (best, rank)
+        
+        # return best
+
+        deg = [0] * n
+        connected = set()
+
+        for a , b in roads:
+            deg[a] += 1
+            deg[b] += 1
+            connected.add((min(a,b), max(a,b)))
+
         best = 0
         for u in range(n):
             for v in range(u+1, n):
-                rank = 0
-                for a, b in roads:
-                    if a == u or a == v or b == u or b == v:
-                        rank += 1
-                
-                best = max (best, rank)
+                rank = deg[u] + deg[v] - ((u,v) in connected)
+                best = max(best, rank)
         
         return best
-        
